e31ea02decdd8a62ccc8a6beab888a536e002d3a
Ganesh Kumar <id.ganesh123@gmail.com>
2018-09-06 03:44:31 +0530

    importing components and methods to work on

--- chat-app/src/app/chat/chat.module.ts
+++ chat-app/src/app/chat/chat.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {path:'chat',component:ChatBoxComponent}
+    ])
   ],
   declarations: [ChatBoxComponent]
 })
